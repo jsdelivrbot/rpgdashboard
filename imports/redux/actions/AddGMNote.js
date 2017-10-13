ce7387077c0c78f20ce2d1f7c4726f12525bcaf0
Douglass Hooker <DouglassHooker@gmail.com>
2017-10-13 18:06:02 -0500

    You can now add a note to the GM notes component

--- imports/redux/actions/AddGMNote.js
+++ imports/redux/actions/AddGMNote.js
@@ -1,8 +1,7 @@
 export function addGMNote(note) {
-
     //Actions must return an object with a 'type' property
     return {
         type: 'NOTE_ADDED',
-        payload: note
+        payload: note,
     }
 }
--- imports/redux/reducers/GMNotesReducer.js
+++ imports/redux/reducers/GMNotesReducer.js
@@ -1,8 +1,10 @@
 //State is not app state, only this reducer's object
-export default function(state, action) {
-    console.log(action);
-    return [
-        {note: 'Testing Testing'},
-        {note: 'Read all about it'}
-    ]
+export default function(state = [], action) {
+    //Add note to state on action
+    switch (action.type) {
+        case 'NOTE_ADDED':
+            //Using ES6 spread to maintain state without mutation
+            return [...state, action.payload]
+    }
+    return state;
 }
