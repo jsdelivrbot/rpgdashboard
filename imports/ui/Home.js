0940bc4461654ab947b8b561be45af709fd6c645
Douglass Hooker <DouglassHooker@gmail.com>
2017-10-13 18:15:56 -0500

    Created Active game details reducer, changed title font

--- imports/ui/Home.js
+++ imports/ui/Home.js
@@ -4,7 +4,8 @@ export default class Home extends Component {
     render() {
         return(
             <div>
-                <h1>Welcome to RPGDashboard!</h1>
+                <h1>RPGDashboard </h1>
+                <h3>We take your campaigns to the digital age</h3>
             </div>
         )
     }
